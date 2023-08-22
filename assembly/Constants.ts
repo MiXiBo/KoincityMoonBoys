6fe0b12ea6ad8efa044dd856f3928302b855cf40
MiXiBo <34360898+MiXiBo@users.noreply.github.com>
2023-08-22 20:28:21 +0200

    Update Constants.ts

--- assembly/Constants.ts
+++ assembly/Constants.ts
@@ -6,7 +6,7 @@ export namespace Constants {
   export const MINT_PRICE: u64 = 0;
   export const MINT_FEE: bool = false;
   export const MAX_SUPPLY: u64 = 75;
-  export const URI: string = "https://bafybeiftt7vwvvhxkud7qsfnfe2u26evae4ugdzknha2fsxizyicgqi74y.ipfs.nftstorage.link/";
+  export const URI: string = "https://bafybeididpafbao4s4xwaljndgtjc5it7kzxboftxm47vnrnwokfinlklq.ipfs.nftstorage.link/";
   export const OWNER: Uint8Array = Base58.decode("1AhGbSHUVaTWV1oqJRSTihsi2ofEvvYevg");
 
   // token mint
